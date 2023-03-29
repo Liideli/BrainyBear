d3dbaf1f6004e52fd55e336c751ece6a8317fb15
Liideli <roope.kasperi.laine@gmail.com>
2023-03-29 12:18:45 +0300

    First commit

---
+++ BrainyBear/BrainyBearApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainyBearApp.swift
+//  BrainyBear
+//
+//  Created by Roope Laine on 29.3.2023.
+//
+
+import SwiftUI
+
+@main
+struct BrainyBearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
