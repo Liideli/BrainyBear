d96521cb6f1de2ef5f8ed80620b8139d7ce050d3
emil <emilsuuronen@gmail.com>
2023-04-20 13:29:45 +0300

    add initial reward store

---
+++ BrainyBear/ShopView.swift
@@ -0,0 +1,20 @@
+//
+//  ShopView.swift
+//  BrainyBear
+//
+//  Created by iosdev on 20.4.2023.
+//
+
+import SwiftUI
+
+struct ShopView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShopView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShopView()
+    }
+}
