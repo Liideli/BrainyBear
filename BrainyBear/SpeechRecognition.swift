11cfeb1bc34e7d7e4f65f3d6fcca95a89d50929f
Nafisul Nazrul <nafisul.tushar@gmail.com>
2023-04-23 20:54:57 +0300

    Speech recognition

---
+++ BrainyBear/SpeechRecognition.swift
@@ -0,0 +1,20 @@
+//
+//  SpeechRecognition.swift
+//  BrainyBear
+//
+//  Created by Tushar Nafisul on 23.4.2023.
+//
+
+import SwiftUI
+
+struct SpeechRecognition: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SpeechRecognition_Previews: PreviewProvider {
+    static var previews: some View {
+        SpeechRecognition()
+    }
+}
